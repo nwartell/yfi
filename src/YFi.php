dcc27ffc74617bbc988a739448b9eef4b9b67207
nwartell <50269968+nwartell@users.noreply.github.com>
2024-04-11 15:55:37 -0400

    More metadata
    Added options endpoint support and methods to pull new metadata from it. shortName, longName, marketChange, fiftyTwoWeekRange.

--- src/YFi.php
+++ src/YFi.php
@@ -17,6 +17,8 @@ class YFi {
         $this->data = $this->init();
     }
 
+    /* CHART Endpoint */
+
     private function init(): ?array {
         $json = file_get_contents("https://query1.finance.yahoo.com/v8/finance/chart/$this->symbol?region=US&lang=en-US&includePrePost=false&interval=$this->interval&useYfid=true&range=$this->range");
         $array = json_decode($json, true);
@@ -30,6 +32,38 @@ class YFi {
     public function marketPrice(): float {
         $meta = $this->meta();
         return $meta['regularMarketPrice'];
+    }
+
+    /* OPTIONS Endpoint */
+
+    public function options(): ?array {
+        $json = file_get_contents("https://query1.finance.yahoo.com/v6/finance/options/$this->symbol");
+        $array = json_decode($json, true);
+        return $array['optionChain']['result'][0];
+    }
+
+    public function optionsQuote(): ?array {
+        return $this->options()['quote'];
+    }
+
+    public function shortName(): string {
+        $options = $this->optionsQuote();
+        return $options['shortName'];
+    }
+
+    public function longName(): string {
+        $options = $this->optionsQuote();
+        return $options['longName'];
+    }
+
+    public function marketChange(): float {
+        $options = $this->optionsQuote();
+        return $options['regularMarketChange'];
+    }
+
+    public function fiftyTwoWeekRange(): string {
+        $options = $this->optionsQuote();
+        return $options['fiftyTwoWeekRange'];
     }
 
     /* Old Methods */
