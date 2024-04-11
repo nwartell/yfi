dbe881683c20f5a905f12d9168afb2aa20260fbb
nwartell <50269968+nwartell@users.noreply.github.com>
2024-04-11 11:28:34 -0400

    New methods

--- src/YFi.php
+++ src/YFi.php
@@ -4,6 +4,36 @@ namespace Nwartell\YFi;
 
 class YFi {
 
+    public $symbol;
+    public $interval;
+    public $range;
+    public $data;
+
+    function __construct(string $symbol, string $interval, string $range) {
+        $this->symbol = $symbol;
+        $this->interval = $interval;
+        $this->range = $range;
+
+        $this->data = $this->init();
+    }
+
+    private function init(): ?array {
+        $json = file_get_contents("https://query1.finance.yahoo.com/v8/finance/chart/$this->symbol?region=US&lang=en-US&includePrePost=false&interval=$this->interval&useYfid=true&range=$this->range");
+        $array = json_decode($json, true);
+        return $array['chart']['result'][0];
+    }
+
+    public function meta(): array {
+        return $this->data['meta'];
+    }
+
+    public function marketPrice(): float {
+        $meta = $this->meta();
+        return $meta['regularMarketPrice'];
+    }
+
+    /* Old Methods */
+
     public static function quote($symbol, $interval, $range) {
         $json = file_get_contents("https://query1.finance.yahoo.com/v8/finance/chart/$symbol?region=US&lang=en-US&includePrePost=false&interval=$interval&useYfid=true&range=$range");
         
